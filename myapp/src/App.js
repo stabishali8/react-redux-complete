70257880d2eab4e2601fcd3d9ff9483a66068f6a
stabishali8 <stabishali8@gmail.com>
2020-12-29 01:29:24 +0500

    Complete React Tutorial (& Redux) #14 - Outputting Lists

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,24 @@
 import Ninjas from './Ninjas';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My First React App</h1>
-      <p>Welcome</p>
-      <Ninjas name="Tabish Ali" age="25" belt="black"/>
-      <Ninjas name="Irtaza Ali" age="1 month" belt="blue"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Tabish', age: 26, belt: 'black', id: 1 },
+      { name: 'Irtaza', age: 1, belt: 'blue', id: 2 },
+      { name: 'Jiya', age: 22, belt: 'red', id: 3 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>My First React App</h1>
+        <p>Welcome</p>
+        <Ninjas ninjas={this.state.ninjas} />
+      </div>
+    );
+  }
 }
 
 export default App;
