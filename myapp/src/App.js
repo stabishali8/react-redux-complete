2a27d5fe351c4dd722ded7b193fad3e4d0b12204
stabishali8 <stabishali8@gmail.com>
2020-12-29 01:13:00 +0500

    Complete React Tutorial (& Redux) #13 - Props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,7 +5,8 @@ function App() {
     <div className="App">
       <h1>My First React App</h1>
       <p>Welcome</p>
-      <Ninjas />
+      <Ninjas name="Tabish Ali" age="25" belt="black"/>
+      <Ninjas name="Irtaza Ali" age="1 month" belt="blue"/>
     </div>
   );
 }
