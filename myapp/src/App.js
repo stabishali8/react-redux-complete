8698c7151332fb341b4f04f457c1688ba33b51a8
stabishali8 <stabishali8@gmail.com>
2020-12-29 00:51:06 +0500

    Complete React Tutorial (& Redux) #12 - Nesting Components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,11 @@
+import Ninjas from './Ninjas';
 
 function App() {
   return (
     <div className="App">
       <h1>My First React App</h1>
       <p>Welcome</p>
+      <Ninjas />
     </div>
   );
 }
