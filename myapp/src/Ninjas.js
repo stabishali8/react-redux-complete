4d86e4ae1aa5d13f37d4f42c8f6d18b3279642ea
stabishali8 <stabishali8@gmail.com>
2020-12-30 02:34:18 +0500

    add funational

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 const Ninjas = ({ninjas}) => {
 
     const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-            </div>
-        )
+        if(ninja.age > 20){
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                </div>
+            )
+        }
     })
     return ( 
         <div className="ninja-list">
