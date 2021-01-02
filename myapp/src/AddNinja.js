1aaf35d01e8c53a2f8a3a948f17267de7b751af1
stabishali8 <stabishali8@gmail.com>
2021-01-03 03:01:03 +0500

    Complete React Tutorial (& Redux ) #17 - Forms Revisited

---
+++ myapp/src/AddNinja.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class AddNinja extends Component {
+    state = {
+        name: null,
+        age: null,
+        belt: null
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+    hanldeSubmit = (e) =>{
+        e.preventDefault();
+        console.log(this.state);
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.hanldeSubmit}>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" onChange={this.handleChange}></input>
+                    <label htmlFor="name">Age:</label>
+                    <input type="text" id="age" onChange={this.handleChange}></input>
+                    <label htmlFor="name">Belt:</label>
+                    <input type="text" id="belt" onChange={this.handleChange}></input>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddNinja;
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import Ninjas from './Ninjas';
 import React, { Component } from 'react';
+import AddNinja from './AddNinja'
 
 class App extends Component {
   state = {
@@ -16,6 +17,7 @@ class App extends Component {
         <h1>My First React App</h1>
         <p>Welcome</p>
         <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja/>
       </div>
     );
   }
